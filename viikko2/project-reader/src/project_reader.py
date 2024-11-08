e5461d5f8e0ba24eeef60c2e57061aa15b36a488
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-11-08 14:53:24 +0200

    Viikko 2 tehtävät 1 ja 2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,3 +1,4 @@
+import toml
 from urllib import request
 from project import Project
 
@@ -9,7 +10,33 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        parsed_content = toml.loads(content)
+        unknown = "Unknown"
+        try:
+            name = parsed_content["tool"]["poetry"]["name"]
+        except KeyError:
+            name = unknown
+        try:
+            desciption = parsed_content["tool"]["poetry"]["description"]
+        except KeyError:
+            desciption = unknown
+        try:
+            dependencies = [i for i in parsed_content["tool"]["poetry"]["dependencies"]]
+        except KeyError:
+            dependencies = unknown
+        try:
+            dev_dependencies = [j for j in parsed_content["tool"]["poetry"]["group"]["dev"]["dependencies"]]
+        except KeyError:
+            dev_dependencies = unknown
+        try:
+            licence = parsed_content["tool"]["poetry"]["license"]
+        except KeyError:
+            licence = unknown
+        try:
+            authors = [x for x in parsed_content["tool"]["poetry"]["authors"]]
+        except KeyError:
+            authors = unknown
+        
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, desciption, licence, authors, dependencies, dev_dependencies)
