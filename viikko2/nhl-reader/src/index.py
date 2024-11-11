b5157b681072ac843fec0cb10fecb38c58eaedd6
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-11-11 13:29:54 +0200

    Tehtävä 10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,18 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+    players = []
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == 'FIN':
+            players.append(player)
+    print("Players from FIN:")
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
