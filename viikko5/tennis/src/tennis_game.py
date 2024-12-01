83aa7ff73c7b4b723a629c1a9c42ee619f67c40c
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-12-01 19:56:33 +0200

    Viikko 5 tehtävä 3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,55 @@
+DICT = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty", 4: "Deuce"}
+
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.score1 = 0
+        self.score2 = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.score1 += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.score2 += 1
 
-    def get_score(self):
+    def even(self, score1):
+        score = ""
+        if score1 >= 3:
+            return "Deuce"
+        score = DICT[score1] + "-All"
+        return score
+
+    def winning(self, score1, score2):
+        whos_winning = score1 - score2
+
+        if whos_winning == 1:
+            score = "Advantage " + self.player1_name
+        elif whos_winning == -1:
+            score = "Advantage " + self.player2_name
+        elif whos_winning >= 2:
+            score = "Win for " + self.player1_name
+        else:
+            score = "Win for " + self.player2_name
+        return score
+
+    def middle_game(self, score1, score2):
         score = ""
         temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
+        for i in (score1, score2):
+            score += DICT[i]
+            if temp_score == 0:
+                score += "-"
+                temp_score += 1
         return score
+
+    def get_score(self):
+        if self.score1 == self.score2:
+            return self.even(self.score1)
+
+        elif self.score1 >= 4 or self.score2 >= 4:
+            return self.winning(self.score1, self.score2)
+
+        else:
+            return self.middle_game(self.score1, self.score2)
